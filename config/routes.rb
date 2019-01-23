48b56199998b5ce87ce618b7d8e3ac33f0050ff9
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-23 17:05:14 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :students, only: [:index, :new]
-  get '/student/:id', to: 'students#show'
+  get '/student/:id', to: 'students#show' as: :student_path
   post 'students', to: 'students#create'
 end
